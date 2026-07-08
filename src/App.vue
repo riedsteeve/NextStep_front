aa49fedd8cab50dbcc5ec28c73e8465818051587
riedsteeve <145045537+riedsteeve@users.noreply.github.com>
2026-07-09 01:12:51 +0200

    Mise en places des analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
-    <router-view></router-view>
+  <router-view></router-view>
+  <Analytics />
 </template>
-
-
